d8c55ec4a7d72885f4a37e296eb583e74a2ea3cd
Pierre Da Silva <pierre.dasilva@ynov.com>
2020-03-21 17:07:27 +0100

    backup

---
+++ Backuplaravelbon/laravel/resources/js/components/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from '@material-ui/core/Slider';
+import Header from './Header';
+
+function App() {
+    const [value, setValue] = React.useState([20, 37]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
+        <div className="container">
+            <Header />
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                        <Slider
+                            value={value}
+                            onChange={handleChange}
+                            valueLabelDisplay="auto"
+                            aria-labelledby="range-slider"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}

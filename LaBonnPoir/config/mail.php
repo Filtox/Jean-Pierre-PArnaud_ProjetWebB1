a84c8752cc013c4e85e4193021e2c710c5bf622c
Filtox <pierre-du-12@hotmail.fr>
2020-04-24 11:03:28 +0200

    maj

--- LaBonnPoir/config/mail.php
+++ LaBonnPoir/config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'pierredasilva19@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'labonnpoir@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'LaBonnPoir'),
     ],
 
